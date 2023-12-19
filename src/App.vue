1fd065d205b4d63ee37570bb8b1066d74b36090a
Agrim Sharma <56984222+agrimreaper49@users.noreply.github.com>
2023-12-18 23:23:28 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container">
+      <h1>Agrim Sharma</h1>
+      <p>UVA Computer Science Student. Aspiring Software Engineer</p>
+      <a href="https://github.com/agrimreaper49" target="_blank">
+        <i class="fab fa-github social-icon"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/agrim-sharma571/" target="_blank">
+        <i class="fab fa-linkedin social-icon"></i>
+      </a>
+      <a href="/resume.pdf" target="_blank">
+        <i class="fas fa-file-alt social-icon"></i>
+      </a>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
+.social-icon {
+  font-size: 1.5em; /* Adjust size as needed */
+  margin-right: 10px; /* Space between icons */
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.container {
   margin-top: 60px;
 }
+
+h1 {
+  font-family: 'Rubik', sans-serif;
+  font-size: 2em;
+  margin-bottom: 0;
+}
+
+p {
+  font-family: 'Rubik', sans-serif;
+  font-size: 1.2em;
+  color: #34495e;
+}
 </style>
+
